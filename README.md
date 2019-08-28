a1a86ac4019a8478a40fdea6827a832d17827eaa
Robert Kleger <robert.kleger@bobb.io>
2019-08-28 13:23:15 +0200

    🎉 init

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">Node Playground</h1>
+
+<br />
+
+![divider](./divider.png)
---
+++ test/unit/dummy.test.ts
@@ -0,0 +1,5 @@
+describe('dummy test suite', () => {
+  test('dummy test', () => {
+    expect(1).toEqual(1);
+  });
+});
